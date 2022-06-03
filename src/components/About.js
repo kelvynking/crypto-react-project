f43877631fa5eb98a188cb2473c95fcc359e08e0
Kelvyn King <kelvyn.m.king@gmail.com>
2022-06-03 22:28:40 +1000

    Updated About Page

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,17 @@ function About(props) {
   return (
     <div className="container-fluid p-3">
       <h1>About this app</h1>
-      <p>Here is the about in page</p>
+      <p>
+        This project has been created to allow people to view the most popular
+        crypto currencies there are at the moment. Users can view all the
+        currencies and then click on any of them to see more information.
+      </p>
+      <p>
+        When users are logged into the app, they can save comments about the
+        currencies and reload those comments to view their previous comments.
+        Comments can be deleted by the people who have written them but can be
+        viewed by all people.
+      </p>
     </div>
   );
 }
